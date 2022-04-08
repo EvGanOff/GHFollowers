bed1f90e7a4936c4a4e1fa666292f2ce8a376938
Evgeny Ganusenko <evgan66@gmail.comevgan66@gmail.comevgan66@gmail.com>
2022-04-08 11:13:47 +0300

    create class NetworkManager with native SDK URLSession

--- GHFollowers/Sourse/Controllers/FolloverListViewController.swift
+++ GHFollowers/Sourse/Controllers/FolloverListViewController.swift
@@ -16,6 +16,17 @@ class FolloverListViewController: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
 
+        // Наш Singlton для запроса в сеть
+        NetworkManager.shared.getFollowes(userName: userName, page: 1) { followers, errorMessage in
+            guard let follower = followers else {
+                self.presentsGFAlertControllerOnMainTread(title: "Bad stuff", massage: errorMessage!, buttonTitle: "OK")
+                return
+            }
+
+            print("Количество подписчиков = \(followers?.count ?? 0)")
+            print(followers ?? [])
+        }
+
     }
 
     override func viewWillAppear(_ animated: Bool) {
