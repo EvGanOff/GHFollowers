c26f6bac9a35d53079fa5c0db3f36c2308f771cc
Evgeny Ganusenko <evgan66@gmail.comevgan66@gmail.comevgan66@gmail.com>
2022-04-25 01:02:26 +0300

    Child View Controllers
    Настройка других контейнерных представлений для дочерних контроллеров представлений.

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Евгений Ганусенко on 4/24/22.
+//
+
+import Foundation
+
+struct SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
